ab475f7c753b815d681e2aa27af6afbccd33d0ac
Victor Payno <vpayno@users.noreply.github.com>
2025-05-18 16:22:46 -0700

    nix(fmt): add treefmt config

---
+++ flake.nix
@@ -0,0 +1,33 @@
+# flake.nix
+{
+  description = "Google Coral Workspace";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
+    treefmt-conf = {
+      url = "github:vpayno/nix-treefmt-conf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      flake-utils,
+      treefmt-conf,
+      ...
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+    in
+    flake-utils.lib.eachSystem systems (system: {
+      formatter = treefmt-conf.formatter.${system};
+    });
+}
